91ea75d64d5a2c629df5cf95b60478fcc447095c
Ateios <ateosnochamploo@gmail.com>
2022-10-29 17:55:14 +0200

    finished my project 3

--- js/tokyo.js
+++ js/tokyo.js
@@ -7,9 +7,19 @@ function display(id){
     }
     let element = document.querySelector(id);
     element.classList.remove("hidden-element");
+    localStorage.setItem("idBlockInfo", id);
 }
 
 function close_info(id){
     let element = document.querySelector(id);
     element.classList.add("hidden-element");
+    localStorage.setItem("idBlockInfo", -1);
+}
+
+function load_page(){
+    let idBlockInfo = localStorage.getItem("idBlockInfo");
+    if (idBlockInfo!=null && idBlockInfo!=-1){
+        display(idBlockInfo);
+    }
+
 }
